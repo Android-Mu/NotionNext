72b57f31932d8ea4d192c1ef91a42e4f5a2b8bde
code小生 <1271206271@qq.com>
2026-05-22 00:05:45 +0800

    Update Footer.js

--- themes/gitbook/components/Footer.js
+++ themes/gitbook/components/Footer.js
@@ -1,12 +1,25 @@
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import { siteConfig } from '@/lib/config'
 import SocialButton from './SocialButton'
+
+
+const Footer = () => {
+  return (
+    <footer className='w-full border-t border-gray-200 bg-white dark:border-gray-700 dark:bg-[#1a191d]'>
+      <div className='mx-auto max-w-5xl px-6 py-8 text-sm leading-7 text-gray-500 dark:text-gray-400'>
+        本站所收集资源均来自网盘用户公开分享，不做任何盈利，仅作个人公益学习，其网盘资源文件的有效性和安全性需自行判断，请勿非法，商业传播，如有侵权，请发邮件（1842324670@qq.com）进行投诉，我们将在第一时间删除对应资源和相关文章！
+      </div>
+    </footer>
+  )
+}
+
+
 /**
  * 站点也叫
  * @param {*} param0
  * @returns
  */
-const Footer = ({ siteInfo }) => {
+/**const Footer = ({ siteInfo }) => {
   const d = new Date()
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
@@ -61,6 +74,6 @@ const Footer = ({ siteInfo }) => {
       <h1 className='pt-1 hidden'>{siteConfig('TITLE')}</h1>
     </footer>
   )
-}
+}*/
 
 export default Footer
